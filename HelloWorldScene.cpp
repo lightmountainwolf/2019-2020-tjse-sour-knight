74055879ec42b5c9af9fc7633ec845d43221cd0d
Star-echo <58218242+Star-echo@users.noreply.github.com>
2020-05-24 20:58:42 +0800

    Add files via upload
    改动只又init（）部分
    还没有添加地图间的切换
    想打开另一个地图就改动“ .tmx”里的内容

--- HelloWorldScene.cpp
+++ HelloWorldScene.cpp
@@ -25,11 +25,33 @@
 #include "HelloWorldScene.h"
 #include "SimpleAudioEngine.h"
 
+
 USING_NS_CC;
+
+/*Scene* firstMap::createScene() {
+	Scene* scene = Scene::create();
+	firstMap* layer = firstMap::create();
+	scene->addChild(layer);
+	return scene;
+}
+bool firstMap::init() {
+	TMXTiledMap* tmx = TMXTiledMap::create("firstMap.tmx");//地图
+	addChild(tmx);
+	return true;
+}*/
+/*Scene* HelloWorld::createScene()
+{
+	auto scene = Scene::createWithPhysics();
+	scene->getPhysicsWorld()->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
+	auto layer = HelloWorld::create();
+	scene->addChild(layer);
+    return scene;
+}*/
 
 Scene* HelloWorld::createScene()
 {
-    return HelloWorld::create();
+	
+	return HelloWorld::create();
 }
 
 // Print useful error message instead of segfaulting when files are not there.
@@ -52,54 +74,67 @@ bool HelloWorld::init()
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-	TMXTiledMap* tmx = TMXTiledMap::create("map.tmx");//地图
+	TMXTiledMap* map = TMXTiledMap::create("firstMap.tmx");//地图
 	//改变位置
 	/*tmx->setPosition(visibleSize.width / 2, visibleSize.height / 2);
 	CCLOG("%f,%f", visibleSize.width / 2, visibleSize.height / 2);
 	tmx->setAnchorPoint(Vec2(0.5, 0.5));
 	tmx->setScale(Director::getInstance()->getContentScaleFactor());*/
 	//获取地图属性
-	addChild(tmx, 0);
+	addChild(map, 0);
 	//改变地图大小，参数为倍数
-	//tmx->setScale(0.5);
-	auto mapProperties = tmx->getProperties();
-	auto str = mapProperties["type"].asString();
-	CCLOG("tmx pro:%s", str.c_str());
+	//map->setScale(1.5);
+	//auto mapProperties = map->getProperties();
+	//auto str = mapProperties["type"].asString();
+	//CCLOG("tmx pro:%s", str.c_str());
 	//获取图层属性
-	auto sceneLayer = tmx->getLayer("scene");
+	auto sceneLayer = map->getLayer("ground");
 	auto layerProperties = sceneLayer->getProperties();
 	CCLOG("layer pro:%s", layerProperties["type"].asString().c_str());
 	//获取图块属性
-	auto tilePro = tmx->getPropertiesForGID(1).asValueMap();
-	CCLOG("tile pro:%s", tilePro["type"].asString().c_str());
+	//auto tilePro = map->getPropertiesForGID(1).asValueMap();
+	//CCLOG("tile pro:%s", tilePro["type"].asString().c_str());
 	//获取对象属性
-	auto objectGroup = tmx->getObjectGroup("object");
-	auto obj = objectGroup->getObject("player");
+	auto objectGroup = map->getObjectGroup("object");
+	auto obj = objectGroup->getObject("knight");
 
 	CCLOG("tile pro:%s", obj["type"].asString().c_str());
 
 	//修改指定区域的颜色
-	/*auto colorObj = objectGroup->getObject("color");
+	auto colorObj = objectGroup->getObject("pass");
 	auto colorObjX = colorObj["x"].asInt() / 32;
 	auto colorObjY = colorObj["y"].asInt() / 32;
 	auto colorObjW = colorObj["width"].asInt() / 32;
 	auto colorObjH = colorObj["height"].asInt() / 32;
 	for (int x = colorObjX; x < colorObjX + colorObjW; x++) {
 		for (int y = colorObjY; y < colorObjY + colorObjH; y++) {
-			auto sprite = sceneLayer->getTileAt(Vec2(x, tmx->getMapSize().height-y));
+			auto sprite = sceneLayer->getTileAt(Vec2(x, map->getMapSize().height-y));
 			sprite->setColor(Color3B(225, 0, 0));
 		}
-	}*/
+	}
 
-	auto player = Sprite::create("sc.png");//这个是对象
+	auto player = Sprite::create("fox.png");//这个是对象
 	player->setPosition(obj["x"].asFloat(), obj["y"].asFloat());
 	addChild(player);
+
+	/*LabelTTF*label=LabelTTF::create("show the game","Courier",36);
+	addChild(label);
+	label->setPosition(visibleSize.width / 2, visibleSize.height / 2);
+	EventListenerTouchOneByOne* listener = EventListenerTouchOneByOne::create();
+	listener->onTouchBegan=[label](Touch* t, Event* e) {
+		if (label->getBoundingBox().containsPoint(t->getLocation())) {
+			Director::getInstance()->replaceScene(firstMap::createScene());
+		}
+		return false;
+	};
+	Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, label);
+	*/
 	//这是鼠标监听
 	auto eventListener = EventListenerTouchOneByOne::create();
-	eventListener->onTouchBegan = [tmx, sceneLayer,player](Touch* touch, Event* event) {
-		auto point = tmx->convertToNodeSpace(touch->getLocation());
+	eventListener->onTouchBegan = [map, sceneLayer,player](Touch* touch, Event* event) {
+		auto point = map->convertToNodeSpace(touch->getLocation());
 		point.x = static_cast<int>(point.x / 32);
-		point.y = tmx->getMapSize().height - static_cast<int>(point.y / 32)-1;
+		point.y = map->getMapSize().height - static_cast<int>(point.y / 32)-1;
 
 		auto t = sceneLayer->getTileAt(point);
 		//检查监听是否正确
@@ -107,21 +142,39 @@ bool HelloWorld::init()
 
 		//碰撞设置
 		auto gid = sceneLayer->getTileGIDAt(point);
-		auto p = tmx->getPropertiesForGID(gid).asValueMap();
+		auto p = map->getPropertiesForGID(gid).asValueMap();
 
 		if (p["move"].isNull() != true && p["move"].asBool() == true){
 			player->setPosition(t->getPosition().x, t->getPosition().y);
 			}
-
 		return true;
 	};
-	_eventDispatcher->addEventListenerWithSceneGraphPriority(eventListener, tmx);
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(eventListener, map);
 	
 	
     return true;
 }
+/*
+void HelloWorld::onEnter() {
+	
 
+	addEdges();
 
+}
+
+void HelloWorld::addEdges() {
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+
+	auto body = PhysicsBody::createEdgeBox(visibleSize,PHYSICSBODY_MATERIAL_DEFAULT,3);
+
+	auto edgeShape = Node::create();
+	
+	edgeShape->setPhysicsBody(body);
+	edgeShape->setPosition(visibleSize.width / 2, visibleSize.height / 2);
+	addChild(edgeShape);
+
+}
+*/
 void HelloWorld::menuCloseCallback(Ref* pSender)
 {
     //Close the cocos2d-x game scene and quit the application
