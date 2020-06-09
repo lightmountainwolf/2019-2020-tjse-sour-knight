f34de787eb961b8356581ce4c274a2d6eb486d42
Star-echo <58218242+Star-echo@users.noreply.github.com>
2020-06-09 19:50:58 +0800

    Add files via upload
    放在resourse里面

---
+++ 地图2.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="地图2" tilewidth="32" tileheight="32" tilecount="512" columns="8">
+ <image source="地图2.png" width="256" height="2048"/>
+ <tile id="256">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+</tileset>
